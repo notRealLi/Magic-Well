159f966d038adfd1040dd3b202df7ec020f32aa7
Li <liyunwei10@gmail.com>
2020-08-10 20:48:52 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import GetOldTweets3 as got
+import os
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/tweets/search', methods=['GET'])
+def search_tweets():
+    query = request.args.get('keywords')
+    count = 100
+    criteria = got.manager.TweetCriteria().setQuerySearch(query).setMaxTweets(count)
+    tweets = got.manager.TweetManager.getTweets(criteria)
+    results = [tweet.text for tweet in tweets]
+    res = jsonify(results)
+
+    return res
+
+
+# Run server
+if __name__ == '__main__':
+    app.run(debug=True)
