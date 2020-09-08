6567779b3451db9117a469a74a4730f0bef68980
Li <liyunwei10@gmail.com>
2020-09-08 12:02:21 -0400

    clean text before predicting

--- ai/sentiment.py
+++ ai/sentiment.py
@@ -5,6 +5,9 @@ class SentimentCalculator:
     def __init__(self):
         self.SENTIMENT_THRESHOLDS = (0.4, 0.7)
 
+    def _clean_text(self, text):
+        return text.replace("'", "")
+
     def _calculate_sentiment(self, score):
         label = 'NEUTRAL'
         if score <= self.SENTIMENT_THRESHOLDS[0]:
@@ -15,6 +18,7 @@ class SentimentCalculator:
         return label
 
     def predict(self, text, model, tokenizer):
+        text = self._clean_text(text)
         x = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=300)
         score = model.predict([x])[0]
         label = self._calculate_sentiment(score)
